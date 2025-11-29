875cf913415e0144b7701ba3abf4629a0467407a
Hadrien Grasland <hadrien.grasland@gmx.fr>
2025-11-29 18:13:31 +0100

    Remove connection options refcounting since we're not doing parallel connections after all

--- src/connect.c
+++ src/connect.c
@@ -38,19 +38,13 @@ connect_options_allocator_initialize() {
     const uint32_t initial_availability = initial_availability_mask();
     debugf("Initializing availability mask to %#x...", initial_availability);
     atomic_init(&allocator.availability, initial_availability);
-
-    for (size_t i = 0; i < NUM_CONNECT_OPTIONS; ++i) {
-        tracef("Initializing reference count of options[%zu]...", i);
-        atomic_init(&allocator.options[i].reference_count, 0);
-    }
     return allocator;
 }
 
 UDIPE_NON_NULL_ARGS
 UDIPE_NON_NULL_RESULT
 shared_connect_options_t*
-connect_options_allocate(connect_options_allocator_t* allocator,
-                         size_t num_target_workers) {
+connect_options_allocate(connect_options_allocator_t* allocator) {
     debug("Looking for unused options that we can allocate...");
     uint32_t availability = atomic_load_explicit(&allocator->availability,
                                                  memory_order_relaxed);
@@ -106,34 +100,23 @@ connect_options_allocate(connect_options_allocator_t* allocator,
         }
     } while(false);
     // With this fence, we synchronize with the thread that previously
-    // deallocated these options, and transitively synchronize with every other
-    // thread that previously liberated them.
+    // deallocated these options.
     atomic_thread_fence(memory_order_acquire);
 
     shared_connect_options_t* const result = &allocator->options[option_idx];
-    debugf("Successfully allocated options[%zu] @ %p, setting refcount to %zu...",
-           option_idx, result, num_target_workers);
-    assert((
-        "Options have not been properly deallocated, might still be in use",
-        atomic_load_explicit(&result->reference_count, memory_order_relaxed)
-    ));
-    atomic_store_explicit(&result->reference_count,
-                          num_target_workers,
-                          memory_order_relaxed);
+    debugf("Successfully allocated options[%zu] @ %p.",
+           option_idx, result);
     return result;
 }
 
-/// Deallocate some \ref shared_connect_options_t
-///
-/// This should be done by the thread that decremented the reference count of
-/// some \ref shared_connect_options_t to zero.
-static inline
-void connect_options_deallocate(connect_options_allocator_t* allocator,
-                                shared_connect_options_t* options) {
+UDIPE_NON_NULL_ARGS
+void connect_options_liberate(connect_options_allocator_t* allocator,
+                              shared_connect_options_t* options) {
+    tracef("Marking worker thread as done with options @ %p...", options);
     const size_t options_idx = options - allocator->options;
     const uint32_t bit = 1 << options_idx;
-    // With release ordering here, we ensure that another thread which observes
-    // this availability bit also observes all prior liberations.
+    // With release ordering here, we ensure that our prior accesses to the
+    // options occur before the options are liberated
     const uint32_t previous_availability =
         atomic_fetch_or_explicit(&allocator->availability,
                                  bit,
@@ -152,50 +135,5 @@ void connect_options_deallocate(connect_options_allocator_t* allocator,
         assert(result == 0 || result == 1);
         exit_on_negative((int) result,
                          "Unexpected FUTEX_WAKE_PRIVATE failure");
-    }
-}
-
-UDIPE_NON_NULL_ARGS
-void connect_options_liberate(connect_options_allocator_t* allocator,
-                              shared_connect_options_t* options) {
-    tracef("Marking one worker thread as done with options @ %p...", options);
-    const size_t initial_refcount =
-        atomic_load_explicit(&options->reference_count,
-                             memory_order_relaxed);
-    assert(("Options have been liberated too many times",
-            initial_refcount > 0));
-
-    // While this fast path may seem infrequent, it is actually always taken in
-    // the common scenario where we're setting up sequential connections.
-    if (initial_refcount == 1) {
-        debugf("Deallocating options @ %p using the single-threaded fast path...",
-               options);
-        // With this fence, we synchronize with every other thread that
-        // decremented the refcount with release ordering previously, so that
-        // the final deallocation fetch_or with release ordering encodes all
-        // decrements. This way, those who will later allocate with acquire
-        // ordering will see all of the liberate calls as being performed.
-        atomic_thread_fence(memory_order_acquire);
-        atomic_store_explicit(&options->reference_count,
-                              0,
-                              memory_order_relaxed);
-        connect_options_deallocate(allocator, options);
-        return;
-    }
-
-    tracef("%zu other workers still have access, must use the slow RMW path...",
-           initial_refcount - 1);
-    // Need release ordering here so that the thread that will finally
-    // deallocate fully observes our liberation operation.
-    const size_t previous_refcount =
-        atomic_fetch_sub_explicit(&options->reference_count,
-                                  1,
-                                  memory_order_release);
-
-    if (previous_refcount == 1) {
-        debugf("Deallocating options @ %p after the RMW slow path...", options);
-        // Need this fence for the same reason as above
-        atomic_thread_fence(memory_order_acquire);
-        connect_options_deallocate(allocator, options);
     }
 }
