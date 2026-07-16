8a62459aeec6a613d50b593d09b0be28a4be1839
Hadrien Grasland <hadrien.grasland@cnrs.fr>
2026-07-16 17:52:59 +0200

    Migrate future type queries to new logging style

--- src/future/type.h
+++ src/future/type.h
@@ -97,23 +97,25 @@ typedef enum future_type_e /* : _BitInt(4) */ {
 /// This function must be called within a logging scope.
 static inline
 bool future_type_has_dependencies(future_type_t type) {
-    switch (type) {
-    case TYPE_INVALID:
-    case TYPE_CUSTOM:
-    case TYPE_TIMER_ONCE:
-    case TYPE_TIMER_REPEAT:
-        return false;
-    case TYPE_NETWORK_CONNECT:
-    case TYPE_NETWORK_DISCONNECT:
-    case TYPE_NETWORK_SEND:
-    case TYPE_NETWORK_RECV:
-    case TYPE_JOIN:
-    case TYPE_UNORDERED:
-        return true;
-    case NUM_TYPES:
-    default:
-        exit_with_error("Unexpected future type!");
-    }
+    LOGGED_FUNCTION_START("%d", type)
+        switch (type) {
+        case TYPE_INVALID:
+        case TYPE_CUSTOM:
+        case TYPE_TIMER_ONCE:
+        case TYPE_TIMER_REPEAT:
+            return false;
+        case TYPE_NETWORK_CONNECT:
+        case TYPE_NETWORK_DISCONNECT:
+        case TYPE_NETWORK_SEND:
+        case TYPE_NETWORK_RECV:
+        case TYPE_JOIN:
+        case TYPE_UNORDERED:
+            return true;
+        case NUM_TYPES:
+        default:
+            exit_with_error("Unexpected future type!");
+        }
+    LOGGED_FUNCTION_END
 }
 
 /// Truth that a future type involves processing other than awaiting other
@@ -125,23 +127,25 @@ bool future_type_has_dependencies(future_type_t type) {
 /// This function must be called within a logging scope.
 static inline
 bool future_type_has_processing(future_type_t type) {
-    switch (type) {
-    case TYPE_NETWORK_CONNECT:
-    case TYPE_NETWORK_DISCONNECT:
-    case TYPE_NETWORK_SEND:
-    case TYPE_NETWORK_RECV:
-    case TYPE_CUSTOM:
-    case TYPE_TIMER_ONCE:
-    case TYPE_TIMER_REPEAT:
-        return true;
-    case TYPE_INVALID:
-    case TYPE_JOIN:
-    case TYPE_UNORDERED:
-        return false;
-    case NUM_TYPES:
-    default:
-        exit_with_error("Unexpected future type!");
-    }
+    LOGGED_FUNCTION_START("%d", type)
+        switch (type) {
+        case TYPE_NETWORK_CONNECT:
+        case TYPE_NETWORK_DISCONNECT:
+        case TYPE_NETWORK_SEND:
+        case TYPE_NETWORK_RECV:
+        case TYPE_CUSTOM:
+        case TYPE_TIMER_ONCE:
+        case TYPE_TIMER_REPEAT:
+            return true;
+        case TYPE_INVALID:
+        case TYPE_JOIN:
+        case TYPE_UNORDERED:
+            return false;
+        case NUM_TYPES:
+        default:
+            exit_with_error("Unexpected future type!");
+        }
+    LOGGED_FUNCTION_END
 }
 
 /// Truth that a future type is awaited under the protection of `lazy_lock`
@@ -155,28 +159,30 @@ bool future_type_has_processing(future_type_t type) {
 /// This function must be called within a logging scope.
 static inline
 bool future_type_uses_lazy_lock(future_type_t type) {
-    switch (type) {
-    case TYPE_INVALID:
-    case TYPE_NETWORK_CONNECT:  // Aliases TYPE_NETWORK_START
-    case TYPE_NETWORK_DISCONNECT:
-    case TYPE_NETWORK_SEND:
-    case TYPE_NETWORK_RECV:
-    case TYPE_CUSTOM:  // Aliases TYPE_NETWORK_END
-    case TYPE_TIMER_ONCE:
-        return false;
-    #ifdef __linux__
-        case TYPE_JOIN:
-        case TYPE_UNORDERED:
-        case TYPE_TIMER_REPEAT:
-            return true;
-    #else
-        // TODO: Add windows versions
-        #error "Sorry, we don't support your operating system yet. Please file a bug report about it!"
-    #endif
-    case NUM_TYPES:
-    default:
-        exit_with_error("Unexpected future type!");
-    }
+    LOGGED_FUNCTION_START("%d", type)
+        switch (type) {
+        case TYPE_INVALID:
+        case TYPE_NETWORK_CONNECT:  // Aliases TYPE_NETWORK_START
+        case TYPE_NETWORK_DISCONNECT:
+        case TYPE_NETWORK_SEND:
+        case TYPE_NETWORK_RECV:
+        case TYPE_CUSTOM:  // Aliases TYPE_NETWORK_END
+        case TYPE_TIMER_ONCE:
+            return false;
+        #ifdef __linux__
+            case TYPE_JOIN:
+            case TYPE_UNORDERED:
+            case TYPE_TIMER_REPEAT:
+                return true;
+        #else
+            // TODO: Add windows versions
+            #error "Sorry, we don't support your operating system yet. Please file a bug report about it!"
+        #endif
+        case NUM_TYPES:
+        default:
+            exit_with_error("Unexpected future type!");
+        }
+    LOGGED_FUNCTION_END
 }
 
 /// Truth that a future type is meant to be asynchronously signaled by a worker
@@ -208,31 +214,33 @@ bool future_type_uses_lazy_lock(future_type_t type) {
 /// This function must be called within a logging scope.
 static inline
 bool future_type_uses_worker_thread(future_type_t type) {
-    switch (type) {
-    case TYPE_NETWORK_CONNECT:  // Aliases TYPE_NETWORK_START
-    case TYPE_NETWORK_DISCONNECT:
-    case TYPE_NETWORK_SEND:
-    case TYPE_NETWORK_RECV:
-        // TODO: Answer is backend-specific as inline backend performs
-        //       operations directly on udipe_start_. Use backend
-        //       infrastructure, once available, to answer this.
-        exit_with_error("Not implemented yet!");
-    case TYPE_CUSTOM:  // Aliases TYPE_NETWORK_END
-        return true;
-    #ifdef __linux__
-        case TYPE_JOIN:
-        case TYPE_UNORDERED:
-        case TYPE_TIMER_REPEAT:
+    LOGGED_FUNCTION_START("%d", type)
+        switch (type) {
+        case TYPE_NETWORK_CONNECT:  // Aliases TYPE_NETWORK_START
+        case TYPE_NETWORK_DISCONNECT:
+        case TYPE_NETWORK_SEND:
+        case TYPE_NETWORK_RECV:
+            // TODO: Answer is backend-specific as inline backend performs
+            //       operations directly on udipe_start_. Use backend
+            //       infrastructure, once available, to answer this.
+            exit_with_error("Not implemented yet!");
+        case TYPE_CUSTOM:  // Aliases TYPE_NETWORK_END
+            return true;
+        #ifdef __linux__
+            case TYPE_JOIN:
+            case TYPE_UNORDERED:
+            case TYPE_TIMER_REPEAT:
+                return false;
+        #else
+            // TODO: Add windows versions
+            #error "Sorry, we don't support your operating system yet. Please file a bug report about it!"
+        #endif
+        case TYPE_INVALID:
+        case TYPE_TIMER_ONCE:
             return false;
-    #else
-        // TODO: Add windows versions
-        #error "Sorry, we don't support your operating system yet. Please file a bug report about it!"
-    #endif
-    case TYPE_INVALID:
-    case TYPE_TIMER_ONCE:
-        return false;
-    case NUM_TYPES:
-    default:
-        exit_with_error("Unexpected future type!");
-    }
+        case NUM_TYPES:
+        default:
+            exit_with_error("Unexpected future type!");
+        }
+    LOGGED_FUNCTION_END
 }
